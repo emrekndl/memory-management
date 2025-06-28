a428c71b1fe382bb07a40fc8169f1d08257656cc
emrekndl <kondulemre@gmail.com>
2025-06-28 22:34:56 +0300

    enums chapter completed.

--- 4-enums/exercise.h
+++ 4-enums/exercise.h
@@ -2,9 +2,27 @@
 #define exercise_h
 
 typedef enum Color {
-  RED,
-  GREEN,
-  BLUE,
+  RED = 55,
+  GREEN = 176,
+  BLUE = 38,
 } color_t;
+//////
+
+typedef enum {
+  HTTP_BAD_REQUEST = 400,
+  HTTP_UNAUTHORIZED = 401,
+  HTTP_NOT_FOUND = 404,
+  HTTP_TEAPOT = 418,
+  HTTP_INTERNAL_SERVER_ERROR = 500
+} http_error_code_t;
+
+char *http_to_str(http_error_code_t code);
+///////
+
+typedef enum {
+  BIG = 123412341234,
+  BIGGER,
+  BIGGEST,
+} BigNumbers;
 
 #endif
--- 4-enums/main.c
+++ 4-enums/main.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 static MunitResult test_color_enum1(const MunitParameter params[], void *data) {
-  munit_assert_int(RED, ==, 0);
-  munit_assert_int(GREEN, ==, 1);
-  munit_assert_int(BLUE, ==, 2);
+  munit_assert_int(RED, ==, 55);
+  munit_assert_int(GREEN, ==, 176);
+  munit_assert_int(BLUE, ==, 38);
   return MUNIT_OK;
 }
 
@@ -15,18 +15,44 @@ static MunitResult test_color_enum2(const MunitParameter params[], void *data) {
   munit_assert_int(BLUE, !=, 0);
   return MUNIT_OK;
 }
+///////
+
+static MunitResult test_switch_enum(const MunitParameter params[], void *data) {
+
+  munit_assert_string_equal(http_to_str(HTTP_BAD_REQUEST), "400 Bad Request");
+  munit_assert_string_equal(http_to_str(HTTP_UNAUTHORIZED), "401 Unauthorized");
+  munit_assert_string_equal(http_to_str(HTTP_NOT_FOUND), "404 Not Found");
+  munit_assert_string_equal(http_to_str(HTTP_TEAPOT), "418 I Am a Teapot");
+  munit_assert_string_equal(http_to_str(HTTP_INTERNAL_SERVER_ERROR), "500 Internal Server Error");
+  return MUNIT_OK;
+}
+
+static MunitResult test_switch_enum_default(const MunitParameter params[], void *data) {
+  munit_assert_string_equal(http_to_str((http_error_code_t)999), "Unknown HTTP status code");
+  return MUNIT_OK;
+}
+
 static MunitTest tests[] = {
     {"/color_enum1", test_color_enum1, NULL, NULL, MUNIT_TEST_OPTION_NONE,
      NULL},
     {"/color_enum2", test_color_enum2, NULL, NULL, MUNIT_TEST_OPTION_NONE,
      NULL},
     ///////
+    {"/switch_enum", test_switch_enum, NULL, NULL, MUNIT_TEST_OPTION_NONE,
+     NULL},
+    {"/switch_enum_default", test_switch_enum_default, NULL, NULL, MUNIT_TEST_OPTION_NONE,
+     NULL},
     {NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}};
 
 static const MunitSuite suite = {"enums", tests, NULL, 1,
                                  MUNIT_SUITE_OPTION_NONE};
 
 int main(int argc, char *argv[]) {
+
+  printf("The size of BigNumbers is %zu bytes\n", sizeof(BigNumbers));
+  printf("The size of HttpErrorCode is %zu bytes\n", sizeof(http_error_code_t));
+
+  /////////
 
   return munit_suite_main(&suite, NULL, argc, argv);
   // gcc -o test main.c exercise0.c munit/munit.c
